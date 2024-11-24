d208ef50d1d4d77af525db1687b91c01e5892f52
y-srinath <sriyellepeddi@yahoo.com>
2024-11-23 19:01:05 -0500

    Add files via upload
    adding python practice file

---
+++ practice.py
@@ -0,0 +1,30 @@
+# Python Practice
+
+#for i in range (1,10):
+
+#    print(i)
+print("---------------------------------------------------");    
+for i in range (1,10,2):
+
+    print(i)
+print("---------------------------------------------------"); 
+for i in "Hello":
+    #if i =='l':
+    #    continue #try "break"
+    print(i.upper())
+print("---------------------------------------------------"); 
+name = "Srinath Yellepeddi"
+print(len(name)-1)
+print(name.count('e'))
+print(name.removesuffix("Yellepeddi"))
+print(name.removeprefix("Srinath"))
+print("---------------------------------------------------"); 
+list = [1,2,"Hello", 2.0]
+print(list[0])
+print(list[-4])
+print("---------------------------------------------------");
+t1 = (1,2,3)
+t2 = (4,5,6)
+print(type(t1))
+print(t1+t2)
+print("---------------------------------------------------");
